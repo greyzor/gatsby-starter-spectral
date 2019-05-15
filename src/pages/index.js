4695731027af9f5e8a7d3cf3a9804bdf8110cab7
Said Aitmbarek <saitmbarek@aldebaran-robotics.com>
2019-05-15 21:57:30 +0200

    adding Section One.

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,24 @@ class HomeIndex extends React.Component {
                         <a href="#one" className="more scrolly">Learn More</a>
                       </section>
 
+                      <section id="one" className="wrapper style1 special">
+                        <div className="inner">
+                          <header className="major">
+                            <h2>Arcu aliquet vel lobortis ata nisl<br />
+                            eget augue amet aliquet nisl cep donec</h2>
+                            <p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
+                            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
+                          </header>
+                          <ul className="icons major">
+                            <li><span className="icon fa-diamond major style1"><span className="label">Lorem</span></span></li>
+                            <li><span className="icon fa-heart-o major style2"><span className="label">Ipsum</span></span></li>
+                            <li><span className="icon fa-code major style3"><span className="label">Dolor</span></span></li>
+                          </ul>
+                        </div>
+                      </section>
+
+
+
                   </div>
                 </div>
 
