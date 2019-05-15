84cfe3f0a66eb3b9f07efbd22ddbae0db44b693d
Said Aitmbarek <saitmbarek@aldebaran-robotics.com>
2019-05-15 22:10:36 +0200

    fix image import, fixme: sizing not appropriate.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
  
+import pic01 from '../images/pic01.jpg'
+import pic02 from '../images/pic02.jpg'
+import pic03 from '../images/pic03.jpg'
+
 
 class HomeIndex extends React.Component {
 
@@ -61,21 +65,21 @@ class HomeIndex extends React.Component {
 
                       <section id="two" className="wrapper alt style2">
                         <section className="spotlight">
-                          <div className="image"><img src="../images/pic01.jpg" alt="" /></div><div className="content">
+                          <div className="image"><img src={pic01} alt="" /></div><div className="content">
                             <h2>Magna primis lobortis<br />
                             sed ullamcorper</h2>
                             <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
                           </div>
                         </section>
                         <section className="spotlight">
-                          <div className="image"><img src="../images/pic02.jpg" alt="" /></div><div className="content">
+                          <div className="image"><img src={pic02} alt="" /></div><div className="content">
                             <h2>Tortor dolore feugiat<br />
                             elementum magna</h2>
                             <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
                           </div>
                         </section>
                         <section className="spotlight">
-                          <div className="image"><img src="../images/pic03.jpg" alt="" /></div><div className="content">
+                          <div className="image"><img src={pic03} alt="" /></div><div className="content">
                             <h2>Augue eleifend aliquet<br />
                             sed condimentum</h2>
                             <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
